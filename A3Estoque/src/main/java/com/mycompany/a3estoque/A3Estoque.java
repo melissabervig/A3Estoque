f3df7ffcef973b1c2aed1157bb3ba604a8323e61
melissabervig <melissabbervig@gmail.com>
2024-10-21 20:30:24 -0300

    Criei o arquivo

---
+++ A3Estoque/src/main/java/com/mycompany/a3estoque/A3Estoque.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.a3estoque;
+
+/**
+ *
+ * @author melissa
+ */
+public class A3Estoque {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
